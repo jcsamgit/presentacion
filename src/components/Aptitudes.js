5c960f7e9e6e425e760f0d08d219147bdb0db620
jcsamgit <jcv.gerald@gmail.com>
2023-03-30 00:48:49 -0300

    pequeños cambios en la descripcion y css

--- src/components/Aptitudes.js
+++ src/components/Aptitudes.js
@@ -9,7 +9,7 @@ export default function Aptitudes(props) {
             <li>Data Entry (avanzado)</li>
             <li>Manejos de macros(avanzado)</li>
             <li>Word, Excel, Power Point (intermedio-avanzado)</li>
-            <li>Lenguaje C (básico)</li>
+            <li>C++ (básico)</li>
             <li>SQLite(básico)</li>
             <li>Visual Studio Code(avanzado)</li>
             <li>GIT y GitHub (intermedio-avanzado)</li>
--- src/components/Destacado.js
+++ src/components/Destacado.js
@@ -1,49 +1,54 @@
-import React, { useEffect, useState, useRef } from 'react'
-import Aptitudes from './Aptitudes'
-import Educacion from './Educacion'
-import Experiencia from './Experiencia'
-import Logo from './Logo'
-import Proyectos from './Proyectos'
+import React, { useEffect, useState, useRef } from 'react';
+import Aptitudes from './Aptitudes';
+import Educacion from './Educacion';
+import Experiencia from './Experiencia';
+import Logo from './Logo';
+import Proyectos from './Proyectos';
 
 export default function Destacado() {
-    const[mostrar, setMostrar]=useState("Educación")
-    let array=["Educación", "Aptitudes", "Proyectos", "Experiencia"]
+  const [mostrar, setMostrar] = useState('Educación');
+  const [ultimoBoton, setUltimoBoton] = useState('Educación');
+  const array = ['Educación', 'Aptitudes', 'Proyectos', 'Experiencia'];
 
-    const enfoque = useRef(null);
-  
-    useEffect(() => {
-      // Establece el foco en el div cuando se carga la página
-      enfoque.current.focus();
-    }, []);
+  const enfoque = useRef(null);
+
+  useEffect(() => {
+    // Establece el foco en el div cuando se carga la página
+    enfoque.current.focus();
+  }, []);
+
+  const handleClick = (item) => {
+    setMostrar(item);
+    setUltimoBoton(item);
+  };
+
   return (
     <div className='destacado'>
+      <div className='itemLogoContainer'>
+        {array.map((item, index) => (
+          <button
+            key={item.toString()}
+            id={item}
+            className={`itemLogo${ultimoBoton === item ? ' activo' : ''}`}
+            onClick={() => handleClick(item)}
+            ref={index === 0 ? enfoque : null} // referencia solo el primer botón
+          >
+            <p className='parrafoLogo'>{item}</p>
+            {/* <div className='logoContainer'> */}
+            <div className={`logoContainer ${mostrar === item ? 'bbb-activado' : ''}`}>
 
-        <div className='itemLogoContainer'>
-            {
-                array.map((item, index) =>
-                <button
-                    key={item.toString()}
-                    id={item}
-                    className='itemLogo'
-                    onClick={() => setMostrar(item)}
-                    ref={index === 0 ? enfoque : null} // referencia solo el primer botón
-                >
-                    <p className='parrafoLogo'>{item}</p>
-                    <div className='logoContainer'>
-                    <Logo />
-                    </div>
-                </button>
-                )
-            }
-        </div>
+              <Logo />
+            </div>
+          </button>
+        ))}
+      </div>
 
-
-        <div className='contenedorConceptos'>
-            {mostrar=="Educación"?<Educacion/>: false}
-            {mostrar=="Aptitudes"?<Aptitudes/>:false}
-            {mostrar=="Proyectos"?<Proyectos/>:false}
-            {mostrar=="Experiencia"?<Experiencia/>:false}
-        </div>
+      <div className='contenedorConceptos'>
+        {mostrar === 'Educación' ? <Educacion /> : false}
+        {mostrar === 'Aptitudes' ? <Aptitudes /> : false}
+        {mostrar === 'Proyectos' ? <Proyectos /> : false}
+        {mostrar === 'Experiencia' ? <Experiencia /> : false}
+      </div>
     </div>
-  )
+  );
 }
